5399605dc79817c70ab3b170a3f67eab36f77418
chirag2506 <chiraggupta2506@gmail.com>
2020-02-11 11:48:31 +0530

    App added

---
+++ app/src/main/java/com/example/dtufaceapplogin/SignUpFragment.java
@@ -0,0 +1,9 @@
+package com.example.dtufaceapplogin;
+
+import androidx.fragment.app.Fragment;
+
+public class SignUpFragment extends Fragment {
+    private static final String TAG = "SignUpFragment";
+
+
+}
