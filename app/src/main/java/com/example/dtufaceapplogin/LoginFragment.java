c27cf8f83fd214110f3733eb8dc7bdabc307de49
chirag2506 <chiraggupta2506@gmail.com>
2020-02-11 13:25:21 +0530

    UI Almost Done

--- app/src/main/java/com/example/dtufaceapplogin/LoginFragment.java
+++ app/src/main/java/com/example/dtufaceapplogin/LoginFragment.java
@@ -1,6 +1,21 @@
 package com.example.dtufaceapplogin;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class LoginFragment extends Fragment {
+    private static final String TAG = "LoginFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.login_fragment, container, false);
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/dtufaceapplogin/SignUpFragment.java
+++ app/src/main/java/com/example/dtufaceapplogin/SignUpFragment.java
@@ -1,9 +1,23 @@
 package com.example.dtufaceapplogin;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SignUpFragment extends Fragment {
     private static final String TAG = "SignUpFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.signup_fragment, container, false);
+
+        return view;
+    }
 
 
 }
