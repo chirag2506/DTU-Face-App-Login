22807fd2952f6f31c51353e59625597912ad8157
chirag2506 <chiraggupta2506@gmail.com>
2020-02-13 21:05:03 +0530

    Camera permission, request added Because Marshmallow doesn't allow Camera permissions by itself

--- app/src/main/java/com/example/dtufaceapplogin/LoggedInActivity.java
+++ app/src/main/java/com/example/dtufaceapplogin/LoggedInActivity.java
@@ -1,8 +1,14 @@
 package com.example.dtufaceapplogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +27,33 @@ public class LoggedInActivity extends AppCompatActivity {
         startCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent s = new Intent(this, ClickPictures.class);
-                startActivity(s);
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+                    handlePermission();
+                }
             }
         });
+    }
+
+
+    //HANDLING PERMISSION OF CAMERA FOR API > MARSHMALLOW
+
+    private void handlePermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1000);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case 1000:
+                for(int i = 0; i < permissions.length; i++){
+                    String permission = permissions[i];
+                    if(grantResults[i] == PackageManager.PERMISSION_DENIED){
+                        boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+                    }
+            }
+        }
     }
 }
