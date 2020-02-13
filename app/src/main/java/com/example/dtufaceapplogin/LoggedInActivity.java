74f8eb0991425bdf6cbcca1a605b437baff3e94c
chirag2506 <chiraggupta2506@gmail.com>
2020-02-13 19:58:04 +0530

    Updated

--- app/src/main/java/com/example/dtufaceapplogin/LoggedInActivity.java
+++ app/src/main/java/com/example/dtufaceapplogin/LoggedInActivity.java
@@ -2,13 +2,28 @@ package com.example.dtufaceapplogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoggedInActivity extends AppCompatActivity {
+
+    Button startCam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        startCam = (Button) findViewById((R.id.startCam));
+
+        startCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent s = new Intent(this, ClickPictures.class);
+                startActivity(s);
+            }
+        });
     }
 }
